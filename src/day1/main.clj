2e9a0ddf47551eb8ab5c2a5a2d53cf4a5916f295
David Goeke <dg@dgoeke.io>
2020-12-02 16:11:03 -0800

    Day 1 & day 2

---
+++ src/day1/main.clj
@@ -0,0 +1,45 @@
+;; Before you leave, the Elves in accounting just need you to fix your
+;; expense report (your puzzle input); apparently, something isn't quite
+;; adding up. Specifically, they need you to find the two entries that
+;; sum to 2020 and then multiply those two numbers together.
+
+(ns aoc.day1
+  (:require [clojure.edn :as edn]
+            [clojure.java.io :as io]))
+
+(def input
+  (-> "day1-input.edn"
+      io/resource
+      slurp
+      edn/read-string
+      sort))
+
+(defn find-target-sum [sorted-input target]
+  (loop [increasing sorted-input
+         decreasing (reverse sorted-input)]
+    (let [start  (first increasing)
+          end    (first decreasing)
+          result (+ start end)]
+      (cond
+        (= result target) [start end]  ; got it
+        (= start end)     nil          ; no answer found
+        (> result target) (recur increasing (rest decreasing))
+        :else             (recur (rest increasing) decreasing)))))
+
+;; part 1
+(prn :part-1 (apply * (find-target-sum input 2020)))
+
+(defn find-target-sum-triplet [sorted-input target]
+  (loop [items sorted-input]
+    (let [item       (first items)
+          new-target (- target item)
+          result      (find-target-sum sorted-input new-target)]
+      (cond
+        (nil? item)   nil
+        (nil? result) (recur (rest items))
+        :else         [item (first result) (second result)]))))
+
+;; part2
+(prn :part-2 (apply * (find-target-sum-triplet input 2020)))
+
+
---
+++ src/day2/main.clj
@@ -0,0 +1,34 @@
+(ns aoc.day2
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
+
+(def line-regex #"(\d+)-(\d+) (\w): (.*)")
+
+(defn parse-line [line]
+  (let [[_ min max ch pw] (re-matches line-regex line)]
+    {:min (Integer. min)
+     :max (Integer. max)
+     :ch  (first ch)
+     :pw  pw}))
+
+(def input
+  (->> "day2-input.txt"
+       io/resource
+       slurp
+       str/split-lines
+       (map parse-line)))
+
+(defn valid-pw-part1? [{:keys [min max ch pw]}]
+  (let [count (get (frequencies pw) ch)]
+    (and (not (nil? count)) (<= min count) (>= max count))))
+
+(prn :part-1 (->> input (filter valid-pw-part1?) count))
+
+(defn valid-pw-part2? [{:keys [min max ch pw]}]
+  (let [fst (= ch (nth pw (dec min)))
+        snd (= ch (nth pw (dec max)))]
+    (or (and fst (not snd))
+        (and snd (not fst)))))
+
+
+(prn :part-1 (->> input (filter valid-pw-part2?) count))
