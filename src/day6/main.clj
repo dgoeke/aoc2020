295af52faaede3cb7dabd79529c7d626f2fd95f6
David Goeke <dg@github.dgoeke.io>
2020-12-05 22:35:58 -0800

    Add comments

--- src/day6/main.clj
+++ src/day6/main.clj
@@ -9,19 +9,19 @@
       slurp
       str/split-lines
       (->>
-       (partition-by (partial = ""))  ; create subsequences that begin/end at the empty lines
-       (remove (partial = '(""))))))  ; remove empty subseqs (corresponding to blank lines)
+       (partition-by (partial = ""))  ; input is a list of strings per group:
+       (remove (partial = '(""))))))  ;  (("abc" "def" "adf") ("bcd" "bd"))
 
-(prn :part-1
-     (->> input
-          (map (partial reduce (partial reduce conj) #{}))
+(prn :part-1       
+     (->> input                      
+          (map (partial reduce (partial reduce conj) #{}))  ; merge strings into one set per group
           (map count)
           (apply +)))
 
 (prn :part-2
      (->> input
-          (map (partial map (partial into #{})))
-          (map (partial apply set/intersection))
+          (map (partial map (partial into #{})))  ; convert each string to a set
+          (map (partial apply set/intersection))  ; intersect the sets for each group
           (map count)
           (apply +)))
 
