12c6eefb01e1aaba7ff77db32a5f8455e8d9b865
David Goeke <dg@github.dgoeke.io>
2020-12-08 08:51:43 -0800

    Make day 8 more functional

--- src/day8/main.clj
+++ src/day8/main.clj
@@ -9,43 +9,49 @@
        str/split-lines
        (map #(str/split % #" "))
        (map #(update % 0 keyword))
-       (map #(update % 1 read-string))
-       vec))
+       (map #(update % 1 read-string)) ; instructions looks like:
+       vec))                           ;  [[:nop 1] [:jmp -3] ...]
 
+;; step executes the next instruction in the program. it accepts a state
+;; and returns a state, so (step (step (step (step state)))) executes
+;; repeated instructions
+(defn step [{:as state :keys [instructions ip last-ip acc seen result]}]
+  (cond
+    (= ip (count instructions)) (assoc state :result :terminated)
+    (seen ip)                   (assoc state :result :looped)
+    result                      state
+    :else
+    (let [[inst arg] (nth instructions ip)
+          new-state  (-> state
+                         (update :seen conj ip)
+                         (assoc :last-ip ip))]
+      (condp = inst
+        :nop (update new-state :ip inc)
+        :acc (-> new-state (update :ip inc) (update :acc + arg))
+        :jmp (update new-state :ip + arg)))))
+
+;; execute repeatedly iterates the `step` fn until it terminates or loops
 (defn execute [instructions]
-  (loop [ip      0
-         last-ip 0
-         acc     0
-         seen    #{}]
-    (cond
-      (= ip (count instructions)) {:stopped-at ip :last-ip :terminated :accumulator acc}
-      (seen ip)                   {:stopped-at ip :last-ip last-ip :accumulator acc}
-      :else
-      (let [[inst arg] (nth instructions ip)]
-        (condp = inst
-          :nop (recur (inc ip) ip acc (conj seen ip))
-          :acc (recur (inc ip) ip (+ acc arg) (conj seen ip))
-          :jmp (recur (+ ip arg) ip acc (conj seen ip)))))))
+  (let [state {:ip 0 :last-ip 0 :acc 0 :seen #{} :instructions instructions}]
+    (->> (iterate step state)
+         (drop-while (comp nil? :result))
+         first)))
 
-(prn :part-1 (execute instructions))
-
-(defn patch-instructions [index]
-  (let [offset     (->> instructions
-                        (drop index)
-                        (take-while #(= (first %) :acc))
-                        count)
-        new-index (+ offset index)
-        [inst arg] (nth instructions new-index)]
-    [(inc new-index)
-     (assoc instructions new-index [(if (= inst :jmp) :nop :jmp) arg])]))
-
+;; patch-it patches every jmp/nop and executes them in order until it doesn't loop
+;;  `patches` contains a list of offsets/instructions to patch: [[0 :jmp] [7 :nop] ...]
+;;  `patched-programs` is a list of new programs corresponding to each patch
 (defn patch-it []
-  (loop [index                0
-         patched-instructions instructions]
-    (let [{:as result :keys [last-ip accumulator]} (execute patched-instructions)]
-      (if (= last-ip :terminated)
-        result
-        (let [[new-index next-patch] (patch-instructions index)]
-          (recur new-index next-patch))))))
+  (let [patches          (->> instructions
+                              (map-indexed vector)
+                              (filter (fn [[idx [inst _]]] (not= :acc inst)))
+                              (map (juxt first (comp first second))))
+        patch-fn         (fn [offset inst]
+                           (update instructions offset assoc 0 (if (= inst :jmp) :nop :jmp)))
+        patched-programs (map (partial apply patch-fn) patches)]
+    (->> patched-programs
+         (map execute)
+         (drop-while #(= (:result %) :looped))
+         first)))
 
-(prn :part-2 (patch-it))
+(prn :part-1 (:acc (execute instructions)))
+(prn :part-2 (:acc (patch-it)))
