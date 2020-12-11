a3d07ff05ac6e306be5d9982c5f67a3d73ab3cc9
David Goeke <dg@github.dgoeke.io>
2020-12-11 09:22:02 -0800

    Day 11

--- src/day11/main.clj
+++ src/day11/main.clj
@@ -2,52 +2,61 @@
   (:require [clojure.string :as str]
             [clojure.java.io :as io]))
 
-(def lines (->> "day11-input.txt"
-                io/resource slurp
-                str/split-lines) )
+(def lines (->> "day11-input.txt" io/resource slurp str/split-lines))
 
 (def width (count (first lines)))
-(def rows (count lines))
-(def input (str/join "" lines))
+(def input (str/join "" lines))    ; input is a flat list, not 2 dimensional
 (def input-len (count input))
 
-(def empty-val \L)
-(def floor-val \.)
-(def occupied-val \#)
+(defn empty'? [x] (= x \L))
+(defn floor? [x] (= x \.))
+(defn occupied? [x] (= x \#))
 
-(defn empty'? [x] (= x empty-val))
-(defn occupied? [x] (= x occupied-val))
-
-(defn left-edge? [pos] (zero? (mod pos 10)))
+(defn left-edge? [pos] (zero? (mod pos width)))
 (defn right-edge? [pos] (left-edge? (inc pos)))
+(defn above [pos] (if (and pos (>= pos width)) (- pos width)))
+(defn below [pos] (if (and pos (< (+ pos width) input-len)) (+ pos width)))
+(defn left [pos] (if (and pos (not (left-edge? pos))) (dec pos)))
+(defn right [pos] (if (and pos (not (right-edge? pos))) (inc pos)))
 
-(defn above [pos] (when pos (if (> pos width) (- pos width))))
-(defn below [pos] (when pos (if (>= (+ pos width) input-len) nil (+ pos width))))
-(defn left [pos] (if (and pos (not (left-edge? pos)) (> pos 0)) (dec pos)))
-(defn right [pos] (if (and pos (not (right-edge? pos)) (< pos (dec input-len))) (inc pos)))
+(defn look [input pos max-distance direction-fn]
+  (let [[dist result] (some->> (iterate direction-fn pos)
+                               (map-indexed vector)
+                               rest
+                               (drop-while (fn [[_ val]]
+                                             (when val (floor? (nth input val)))))
+                               first)]
+    (if (or (nil? result) (and max-distance (> dist max-distance)))
+      \.
+      (nth input result))))
 
-(defn adjacent-vals [input pos]
-  (->> [(left pos) (right pos) (above pos) (below pos) (above (left pos)) (above (right pos)) (below (left pos)) (below (right pos))]
-       (remove nil?)
-       (map (partial nth input))))
+(defn count-occupied [input max-distance pos]
+  (let [iterators [left right above below
+                   (comp above left) (comp above right) (comp below left) (comp below right)]]
+    (->> iterators
+         (map (partial look input pos max-distance))
+         (filter occupied?)
+         count)))
 
-(defn count-occupied-adjacent [input pos]
-  (count (filter occupied? (adjacent-vals input pos))))
-
-(defn apply-rules [input pos]
+(defn apply-rules [input vision-distance occupied-threshold pos]
   (let [val (nth input pos)]
     (cond
-      (and (empty'? val) (zero? (count-occupied-adjacent input pos)))  occupied-val
-      (and (occupied? val) (>= (count-occupied-adjacent input pos) 4)) empty-val
-      :else                                                            val)))
+      (and (empty'? val) (zero? (count-occupied input vision-distance pos)))   \#
+      (and (occupied? val)
+           (>= (count-occupied input vision-distance pos) occupied-threshold)) \L
+      :else                                                                    val)))
 
-(defn step [input]
-  (mapv (partial apply-rules input) (range input-len)))
+(defn step [count-occupied-fn occupied-threshold input]
+  (mapv (partial apply-rules input count-occupied-fn occupied-threshold)
+        (range input-len)))
 
-(prn :part-1
-     (->> (iterate step input)
-          (partition 2 1)
-          (filter (partial apply =))
-          ffirst
-          (filter #{occupied-val})
-          count))
+(defn final-count [input vision-distance occupied-threshold]
+  (->> (iterate (partial step vision-distance occupied-threshold) input)
+       (partition 2 1)
+       (filter (partial apply =))
+       ffirst
+       (filter #{\#})
+       count))
+
+(prn :part-1 (final-count input 1 4))
+(prn :part-2 (final-count input nil 5))
